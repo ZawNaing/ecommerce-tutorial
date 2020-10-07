5da0eb96c0a247d70b1ad67b8f2be7a52ede5946
ZawNaing <zzawzawnaing07@gmail.com>
2020-10-07 11:22:09 +0630

    End of section 16

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -10,7 +10,28 @@ namespace API.Extensions
             
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkiNet API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Piggy API", Version = "v1" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    {securitySchema, new []{"Bearer"}}
+                };
+                c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;
@@ -20,7 +41,7 @@ namespace API.Extensions
                         app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Piggy API v1");
             });
             return app;
         }
